db009ec090da353daf5ab40d526c0cbaaa0ff112
shuaib shaban <shuaib>
2023-02-08 05:27:44 +0300

    done test

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+    if array.length <= 1
+      return array.first
+    elsif array.length == 2
+      return array.join(" and ")
+    else
+      last_element = array.pop
+      return "#{array.join(", ")}, and #{last_element}"
+    end
+  end
+  
